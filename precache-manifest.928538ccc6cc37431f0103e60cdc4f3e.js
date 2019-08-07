21a6eadd160d3f0ae777d14ef16ee61d70816356
qing <yeqingcanada@gmail.com>
2019-08-06 21:46:52 -0400

    Updates

---
+++ precache-manifest.928538ccc6cc37431f0103e60cdc4f3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb671491239d867c839aa089da34540",
+    "url": "/qing-s-blog/index.html"
+  },
+  {
+    "revision": "3a72e191893b1f3ff274",
+    "url": "/qing-s-blog/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8b921e7e72e1e2eecbb8",
+    "url": "/qing-s-blog/static/css/main.c66f9e5f.chunk.css"
+  },
+  {
+    "revision": "3a72e191893b1f3ff274",
+    "url": "/qing-s-blog/static/js/2.200659af.chunk.js"
+  },
+  {
+    "revision": "f7b83ee2b9d956543e7b",
+    "url": "/qing-s-blog/static/js/3.62ae0050.chunk.js"
+  },
+  {
+    "revision": "c02ae1e409abd4f277cd",
+    "url": "/qing-s-blog/static/js/4.22ec7aac.chunk.js"
+  },
+  {
+    "revision": "8b921e7e72e1e2eecbb8",
+    "url": "/qing-s-blog/static/js/main.d6f8704d.chunk.js"
+  },
+  {
+    "revision": "91bd2015ecd33f0b9adb",
+    "url": "/qing-s-blog/static/js/runtime~main.c0cf0de6.js"
+  }
+]);
